ae7edaddb3cf623bb966a346ee423ad5a8d90aa8
Chris Nady <nady.christopher@gmail.com>
2017-09-12 14:07:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,7 @@ class Song
 
   extend Memorable
   extend Findable
+  include Paramable
 
   @@songs = []
 
@@ -16,10 +17,6 @@ class Song
   def artist=(artist)
     @artist = artist
   end
-
-#  def to_param
-#    name.downcase.gsub(' ', '-')
-#  end
 
   def self.all
     @@songs
